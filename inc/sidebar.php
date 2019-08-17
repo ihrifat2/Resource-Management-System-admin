1d91cc7995a3a93266f73f1a135f5f90a8909fae
Imran Hadid <imranhadid03@gmail.com>
2019-08-17 15:40:05 +0600

    Create sidebar.php

---
+++ inc/sidebar.php
@@ -0,0 +1,32 @@
+        <ul class="sidebar navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="adminSignup.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Admin Registration</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="facultySignup.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Faculty Registration</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="studentinfo.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Student Information</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="facultyinfo.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Faculty Information</span>
+                </a>
+            </li>
+        </ul>
