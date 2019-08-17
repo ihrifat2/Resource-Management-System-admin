1335465f95d4c81da2f1b26b8ffff01d230db05e
Imran Hadid <imranhadid03@gmail.com>
2019-08-17 15:39:53 +0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_SESSION["admin_id"]) && isset($_SESSION["admin_name"])) {
+	session_destroy();
+	header('Location: index.php');
+}else{
+	header('Location: index.php');
+}
+?>
